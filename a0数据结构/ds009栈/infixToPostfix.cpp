11076abe8987de485d9266f2ae2ed7b2e93df0a8
LiAnQing279 <lovingforlife@163.com>
2021-08-20 11:30:46 +0800

    增加求幂运算符

--- a0数据结构/ds009栈/infixToPostfix.cpp
+++ a0数据结构/ds009栈/infixToPostfix.cpp
@@ -16,7 +16,7 @@ using std::string;
 
 bool isOP(char t_ch)
 {
-	return t_ch == '*' || t_ch == '/' || t_ch == '-' || t_ch == '+' || t_ch == '(' || t_ch == ')';
+	return t_ch == '*' || t_ch == '^' || t_ch == '/' || t_ch == '-' || t_ch == '+';
 }
 
 int getNext(int &t_id, const string &t_exp)
@@ -46,12 +46,13 @@ string intToStr(int n)
 /**
  * @brief 这里有个小细节
  * 注意到 str+=char(42)+' ';本质是str+=char(42+32)即先算右边整体字符和，然后转化为一个字符
- * 所以，要分开行相加 ans+=char(42);ans+=' ';
+ * 所以，要分开行相加 ans+=char(42); ans+=' ';
  * 或者，ans += char(stk.top())+string(" "); 注意必须强调string(" ")，否则字符无法加const char*
+ * 或者，map<int, string> ops = {{43, "+"}, {45, "-"}, {42, "*"}, {47, "/"}};用字典查询，也是保证有一个string对象
  */
 string infixToPostfix(const string &t_exp)
 {
-	map<int, int> level = {{'(', 0}, {'+', 1}, {'-', 1}, {'*', 2}, {'/', 2}, {')', 7}};
+	map<int, int> level = {{'+', 1}, {'-', 1}, {'*', 2}, {'/', 2}};
 
 	int i = 0;
 	int n = t_exp.size();
@@ -74,7 +75,7 @@ string infixToPostfix(const string &t_exp)
 				stk.pop(); //丢弃左括号
 				break;
 			default:
-				//弹出高优先级的符号
+				//保证栈顶的优先级是最高的，严格大于下面的符号
 				while (!stk.empty() && level[stk.top()] >= level[nxt]) {
 					ans += char(stk.top()) + string(" ");
 					stk.pop();
@@ -93,11 +94,11 @@ string infixToPostfix(const string &t_exp)
 	}
 	return ans;
 }
-//没搞清楚相加错误，就直接用字典来查符号字符串
-string infixToPostfix1(const string &t_exp)
+//增加求幂运算符
+string infixToPostfix2(const string &t_exp)
 {
-	map<int, int> level = {{'(', 0}, {'+', 1}, {'-', 1}, {'*', 2}, {'/', 2}, {')', 7}};
-	map<int, string> ops = {{43, "+"}, {45, "-"}, {42, "*"}, {47, "/"}};
+	map<int, int> level = {{'^', 3}, {'+', 1}, {'-', 1}, {'*', 2}, {'/', 2}};
+
 	int i = 0;
 	int n = t_exp.size();
 	string ans = "";
@@ -108,20 +109,28 @@ string infixToPostfix1(const string &t_exp)
 		if (isOP(nxt)) {
 			switch (nxt) {
 			case '(':
-				stk.push('(');
+				stk.push(nxt);
+				break;
+			case '^':
+				//如果栈顶为^，保持住不弹出，不是严格大于
+				while (!stk.empty() && level[stk.top()] > level[nxt]) {
+					ans += char(stk.top()) + string(" ");
+					stk.pop();
+				}
+				stk.push(nxt);
 				break;
 			case ')':
 				//匹配括号
 				while (!stk.empty() && stk.top() != '(') {
-					ans += ops[stk.top()] + ' ';
+					ans += char(stk.top()) + string(" ");
 					stk.pop();
 				}
 				stk.pop(); //丢弃左括号
 				break;
 			default:
-				//弹出高优先级的符号
+				//严格保证从栈顶优先级严格大于下面符号
 				while (!stk.empty() && level[stk.top()] >= level[nxt]) {
-					ans += ops[stk.top()] + " ";
+					ans += char(stk.top()) + string(" ");
 					stk.pop();
 				}
 				stk.push(nxt);
@@ -132,19 +141,19 @@ string infixToPostfix1(const string &t_exp)
 		}
 	}
 	while (!stk.empty()) {
-		ans += ops[stk.top()] + ' ';
+		ans += char(stk.top()) + string(" ");
 		stk.pop();
 	}
 	return ans;
 }
-
 int main()
 {
 	string str = "100 - 9 * 10 + ( 20 * ( 11 - 6 ) - 90 ) * 12";
+	string tmp = "10 * ( 10 ^ 1 ^ 2 - 99 ) + 12 ^ 2 ^ 1";
 	int i = 0;
 	// std::cout << getNext(i, str) << std::endl;
 	// std::cout << i << std::endl;
-	std::cout << infixToPostfix(str) << std::endl; //"100 9 10 * - 20 11 6 - * 90 - 12 * +";
-
+	std::cout << infixToPostfix(str) << std::endl;
+	std::cout << infixToPostfix2(tmp) << std::endl;
 	return 0;
 }
